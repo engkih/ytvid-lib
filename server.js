3c6d49b9cc546454fa2b6e3aac73fe64d1eb73ea
engkih <74026663+engkih@users.noreply.github.com>
2023-10-27 09:18:32 +0700

    node:http problem

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const http = require('http');
+const http = require('node:http');
 const app = require('./app');
 
 const port = 3000;
 const server = http.createServer(app);
 
 server.listen(3000);
+
+// const http = require('node:http');
+
+// // Create a local server to receive data from
+// const server = http.createServer();
+
+// // Listen to the request event
+// server.on('request', (request, res) => {
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+//   res.end(JSON.stringify({
+//     data: 'Hello World!',
+//   }));
+// });
+
+// server.listen(8000);
