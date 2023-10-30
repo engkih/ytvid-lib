9793ac708baf1f14450344f0956a82b399f07966
Achmad Riky <achmadricky01@gmail.com>
2023-10-30 13:52:09 +0700

    delete comments

--- server.js
+++ server.js
@@ -5,18 +5,3 @@ const port = 3000;
 const server = http.createServer(app);
 
 server.listen(3000);
-
-// const http = require('node:http');
-
-// // Create a local server to receive data from
-// const server = http.createServer();
-
-// // Listen to the request event
-// server.on('request', (request, res) => {
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   res.end(JSON.stringify({
-//     data: 'Hello World!',
-//   }));
-// });
-
-// server.listen(8000);
